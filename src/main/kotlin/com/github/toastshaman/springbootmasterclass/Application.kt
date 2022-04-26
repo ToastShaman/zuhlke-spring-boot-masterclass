170ba7ad270273258627dbb2e50c55c986a8f9bc
Kevin Denver <kevin.denver@gmail.com>
2022-04-26 11:40:07 +0100

    Rename SpringBootMasterclassApplication > Application

--- src/main/kotlin/com/github/toastshaman/springbootmasterclass/Application.kt
+++ src/main/kotlin/com/github/toastshaman/springbootmasterclass/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringBootMasterclassApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<SpringBootMasterclassApplication>(*args)
+	runApplication<Application>(*args)
 }
