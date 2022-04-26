b52fa180a0f666eefcf3eba45d16fbad2a4d668b
Kevin Denver <kevin.denver@gmail.com>
2022-04-26 09:19:41 +0100

    Use https://start.spring.io/ to initialise the repo

---
+++ src/main/kotlin/com/github/toastshaman/springbootmasterclass/SpringBootMasterclassApplication.kt
@@ -0,0 +1,11 @@
+package com.github.toastshaman.springbootmasterclass
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootMasterclassApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootMasterclassApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/toastshaman/springbootmasterclass/SpringBootMasterclassApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.toastshaman.springbootmasterclass
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootMasterclassApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
