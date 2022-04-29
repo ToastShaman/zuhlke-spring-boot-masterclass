863691cce47fa2f9b84fdf377c0a97dfabfb2104
Kevin Denver <kevin.denver@gmail.com>
2022-04-29 11:16:36 +0100

    Add Validation

--- src/main/kotlin/com/github/toastshaman/springbootmasterclass/ping/api/PingController.kt
+++ src/main/kotlin/com/github/toastshaman/springbootmasterclass/ping/api/PingController.kt
@@ -2,11 +2,23 @@ package com.github.toastshaman.springbootmasterclass.ping.api
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
+import javax.validation.Valid
 
 @RestController
 class PingController {
 
     @GetMapping("/ping")
     fun ping() = ResponseEntity.ok(PingResponse("pong"))
+
+    @PostMapping("/ping/{name}")
+    fun pingWithName(
+        @PathVariable("name") name: String,
+        @Valid @RequestBody pingRequest: PingRequest,
+        request: HttpServletRequest
+    ) = ResponseEntity.ok(PingResponse("""${pingRequest.message} $name"""))
 }
--- src/main/kotlin/com/github/toastshaman/springbootmasterclass/ping/api/PingResponse.kt
+++ src/main/kotlin/com/github/toastshaman/springbootmasterclass/ping/api/PingResponse.kt
@@ -1,3 +1,14 @@
 package com.github.toastshaman.springbootmasterclass.ping.api
 
+import io.micrometer.core.lang.NonNull
+import org.hibernate.validator.constraints.Length
+import javax.validation.constraints.NotEmpty
+
 data class PingResponse(val message: String)
+
+data class PingRequest(
+    @field:NonNull
+    @field:NotEmpty
+    @field:Length(max = 5)
+    val message: String
+)
