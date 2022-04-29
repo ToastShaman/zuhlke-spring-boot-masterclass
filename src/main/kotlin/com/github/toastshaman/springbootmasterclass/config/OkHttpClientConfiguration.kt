82c12db79cb07b50ad418cbe08a2f108725ecc71
Kevin Denver <kevin.denver@gmail.com>
2022-04-29 10:51:54 +0100

    Enable Actuator

--- src/main/kotlin/com/github/toastshaman/springbootmasterclass/config/OkHttpClientConfiguration.kt
+++ src/main/kotlin/com/github/toastshaman/springbootmasterclass/config/OkHttpClientConfiguration.kt
@@ -1,20 +1,34 @@
 package com.github.toastshaman.springbootmasterclass.config
 
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.binder.okhttp3.OkHttpConnectionPoolMetrics
+import io.micrometer.core.instrument.binder.okhttp3.OkHttpMetricsEventListener
 import okhttp3.ConnectionPool
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import okhttp3.logging.HttpLoggingInterceptor.Level.BODY
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeUnit.SECONDS
 
 @Configuration
-class OkHttpClientConfiguration {
+class OkHttpClientConfiguration(
+    private val registry: MeterRegistry
+) {
 
     @Bean
-    fun okHttpClient() = OkHttpClient.Builder()
-        .addInterceptor(HttpLoggingInterceptor(::println).setLevel(BODY))
-        .connectionPool(ConnectionPool(20,60, SECONDS))
-        .build()
+    fun okHttpClient(): OkHttpClient {
+        val connectionPool = ConnectionPool(20, 60, SECONDS)
+            .also { OkHttpConnectionPoolMetrics(it).bindTo(registry) }
+
+        val eventListener = OkHttpMetricsEventListener
+            .builder(registry, "coronavirus")
+            .build()
+
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor(::println).setLevel(BODY))
+            .connectionPool(connectionPool)
+            .eventListener(eventListener)
+            .build()
+    }
 }
