5effc763b9877abe700567740977abd24be9d8f0
Kevin Denver <kevin.denver@gmail.com>
2022-05-12 08:25:38 +0100

    Simplify Faker

--- src/test/kotlin/com/github/toastshaman/springbootmasterclass/covid/domain/MetricEnvelope.kt
+++ src/test/kotlin/com/github/toastshaman/springbootmasterclass/covid/domain/MetricEnvelope.kt
@@ -5,18 +5,17 @@ import java.time.LocalDate
 import kotlin.random.Random
 import kotlin.random.asJavaRandom
 
-fun MetricEnvelope.Companion.random(random: Random = Random(17354)): MetricEnvelope {
-    val faker = Faker(random.asJavaRandom())
+val rnd = Random(17354)
+val defaultFaker = Faker(rnd.asJavaRandom())
 
-    return MetricEnvelope(
-        length = faker.random().nextInt(100).toString(),
-        maxPageLimit = faker.random().nextInt(100).toString(),
-        totalRecords = faker.random().nextInt(100).toString(),
-        data = IntRange(0, faker.random().nextInt(1, 10)).map { DataPoint.random(random) }
-    )
-}
+fun MetricEnvelope.Companion.random(faker: Faker = defaultFaker) = MetricEnvelope(
+    length = faker.random().nextInt(100).toString(),
+    maxPageLimit = faker.random().nextInt(100).toString(),
+    totalRecords = faker.random().nextInt(100).toString(),
+    data = IntRange(0, faker.random().nextInt(1, 10)).map { DataPoint.random(faker) }
+)
 
-fun DataPoint.Companion.random(random: Random = Random(17354)): DataPoint {
-    val faker = Faker(random.asJavaRandom())
-    return DataPoint(LocalDate.EPOCH, faker.random().nextInt(0, 100).toDouble())
-}
+fun DataPoint.Companion.random(faker: Faker = defaultFaker) = DataPoint(
+    date = LocalDate.EPOCH,
+    value = faker.random().nextInt(0, 100).toDouble()
+)
